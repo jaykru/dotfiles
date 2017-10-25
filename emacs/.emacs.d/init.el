ce27bea867365959177cca4bce70f8a1181c7fa2
Jay Kruer <Jay Kruer>
2017-10-25 14:19:14 -0700

    updated emacs config for new nixos install, tweaked some font settings

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,8 @@
+
 ;; enable MELPA
 (require 'package)
 (add-to-list 'load-path "~/.emacs.d/elpa")
+(add-to-list 'load-path "~/.emacs.d/lisp")
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
@@ -19,12 +21,6 @@
 
 ;; quiet, you.
 (setq ring-bell-function 'ignore)
-
-;; no backups
-(setq backup-inhibited t)
-
-;; no autosaving
-(setq auto-save-default nil)
 
 ;; ido-mode
 (ido-mode t)
@@ -45,7 +41,7 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 (setq default-frame-alist
-      '((font . "Courier")
+      '((font . "Inconsolata")
         (vertical-scroll-bars . nil)
         (horizontal-scroll-bars . nil)
         (tool-bar-lines . 0)
@@ -57,9 +53,16 @@
 
 ;; mark theme as safe
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("23ccf46b0d05ae80ee0661b91a083427a6c61e7a260227d37e36833d862ccffc" default))))
+    ("2cf7f9d1d8e4d735ba53facdc3c6f3271086b6906c4165b12e4fd8e3865469a6" "086970da368bb95e42fd4ddac3149e84ce5f165e90dfc6ce6baceae30cf581ef" "23ccf46b0d05ae80ee0661b91a083427a6c61e7a260227d37e36833d862ccffc" default)))
+ '(package-selected-packages
+   (quote
+    (ix wolfram w3m w3 undo-tree typing-game twittering-mode turing-machine threes tao-theme solidity-mode sml-mode smex slime scratches rainbow-delimiters plan9-theme paredit nixos-options nix-sandbox nix-mode moe-theme mingus math-symbol-lists magit-annex latex-preview-pane latex-pretty-symbols japanlaw heroku-theme haskell-mode hacker-typer golden-ratio go-guru go-eldoc go-complete go-autocomplete go gnugo fireplace figlet expand-region deft ciel chess buffer-sets buffer-move basic-mode autotetris-mode auto-complete-auctex auctex 0xc))))
 ;; load it
 (load-theme 'tao-yang)
 
@@ -106,21 +109,12 @@
           (lambda () (progn
                        (rainbow-delimiters-mode t)
                        (paredit-mode t))))
-(setq inferior-lisp-program "sbcl.exe")
+(setq inferior-lisp-program "sbcl")
 (slime-setup '(slime-fancy))
 
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (paredit-mode t)))
-
-(require 'go-autocomplete)
-(setenv "GOPATH" "/home/jaykru/go")
-(add-hook 'go-mode-hook
-          (lambda ()
-            (progn
-              (flycheck-mode)
-              (add-hook 'before-save-hook 'gofmt-before-save)
-              (auto-complete-mode 1))))
 
 (defun my-asm-mode-hook ()
   ;; you can use `comment-dwim' (M-;) for this kind of behaviour anyway
@@ -130,4 +124,10 @@
 
 (add-hook 'asm-mode-hook #'my-asm-mode-hook)
 
-(setq pdf-latex-command "pdflatex")
+(setq pdf-latex-command "pdftex") ; ad fontes!
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

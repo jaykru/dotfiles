a538ceb233131aace00b4af0a694d144d123899c
Jay Kruer <jaykruer@posteo.de>
2019-06-22 18:42:38 -0700

    add bar for kodama

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -1,3 +1,6 @@
-DATE = $(date +'%Y-%m-%d %l:%M:%S %p')
-BAT = $(acpi | cut -d ' ' -f 4 | head -c -1)
-echo $DATE $BAT
+#!/run/current-system/sw/bin/sh
+DATE=$(date +'%Y-%m-%d %l:%M:%S %p')
+BAT0=$(cat /sys/class/power_supply/BAT0/capacity)
+BAT1=$(cat /sys/class/power_supply/BAT1/capacity)
+
+echo $DATE $BAT0% + $BAT1%
